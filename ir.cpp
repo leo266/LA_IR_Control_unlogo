1429db3e051afb2da206db1a86eb73d7b3652078
leo266 <57928651+leo266@users.noreply.github.com>
2019-11-28 15:50:38 +0800

    Update ir.cpp

--- ir.cpp
+++ ir.cpp
@@ -28,27 +28,6 @@ enum class Pins{
 };
 
 enum class RemoteButton {
-      /*//CH_MINUS = 0x45,
-	  UP = 0x46,
-	  //CH_Add = 0x47,
-	  LEFT = 0x44,
-	  RIGHT = 0x43,
-	  NUM3 = 0xd,
-	  OK = 0x40,
-	  NUM2 = 0x19,
-	  //Minus = 0x7,
-	  DOWN = 0x15,
-	  //EQ = 0x9,
-	  NUM1 = 0x16,
-	  NUM4 = 0xc,
-	  NUM5 = 0x18,
-	  NUM6 = 0x5e,
-	  NUM7 = 0x8,
-	  NUM8 = 0x1c,
-	  NUM9 = 0x5a,
-	  STAR= 0x42,
-	  NUM0 = 0x52,
-	  GRID= 0x4A,*/
 	  NUM1 = 0x45,
 	  NUM2 = 0x46,
 	  NUM3 = 0x47,
